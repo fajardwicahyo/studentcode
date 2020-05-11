9111ec8ff5e677f43d07de2e7fc68f20fceac57e
Fajar Dwi Cahyo <44404657+fajardwicahyo@users.noreply.github.com>
2020-05-11 17:14:27 +0800

    Sintaks PHP

---
+++ sintaks.php
@@ -0,0 +1,24 @@
+<?php
+// Pelajaran 4
+// SINTAKS PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var dump
+
+// echo 'Fajar Dwi Cahyo';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<body>
+	<h1>Halo, Selamat datang <?php echo "Fajar"; ?> </h1>
+
+</body>
+</head>
+
+
+
+</html>
