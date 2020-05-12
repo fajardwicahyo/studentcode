f2d1baa4b26b61ef450492d63ae26e5c2c764bcf
Fajar Dwi Cahyo <44404657+fajardwicahyo@users.noreply.github.com>
2020-05-12 18:33:49 +0800

    date

---
+++ pertemuan4/date.php
@@ -0,0 +1,26 @@
+<?php 	
+		echo date("l, d-M-Y");
+		// echo date("d");
+		// echo date("m");
+ ?>
+<!-- <?php
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<table border="1" cellpadding="10" cellspacing="0">
+	<?php for ?>
+	<tr>
+		<td></td>
+
+	</tr>
+
+</table>
+
+</body>
+</html>
