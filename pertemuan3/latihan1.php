e0a1bc56a2ed8cc1ce9951b2d4e1e82756ba859f
Fajar Dwi Cahyo <44404657+fajardwicahyo@users.noreply.github.com>
2020-05-12 18:22:42 +0800

    Latihan 3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,44 @@
+<!-- <?php
+
+// Pengulangan
+// for
+// while
+// do..while
+// foreach : pengulangan khusus array
+
+// for ( $i = 0; $i < 5; $i++ ){
+// 	echo "Hello World! <br>";
+// }
+
+// $i = 0;
+// while ( $i < 5){
+// 	echo "Hello World! <br>";
+// $i++;
+
+// }
+// $i = 10; 
+// do {
+// 	echo "Hello World! <br>";
+// $i++;
+// } while ( $i < 5);
+
+?> -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<table border="1" cellpadding="10" cellspacing="0">
+	<?php for ?>
+	<tr>
+		<td></td>
+
+	</tr>
+
+</table>
+
+</body>
+</html>
