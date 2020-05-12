0c4e022ba4caea98e7d373b76587e3ca705b100c
Fajar Dwi Cahyo <44404657+fajardwicahyo@users.noreply.github.com>
2020-05-12 19:02:47 +0800

    array

---
+++ pertemuan5/latihan1.php
@@ -0,0 +1,16 @@
+<?php 	
+		$hari = ["Senin", "Selasa","Rabu", "Kamis", "Jumat", "Sabtu", "Ahad"];
+		$bulan = array ("Januari", "Februari", "Maret");
+		$array1 = [123, "huruf"' false'];
+
+		// array adalah pasangan dari key dan value
+		// key nya adalah index, dan dimulai dari 0
+
+		var_dump($hari);
+		echo "<br>";
+		print_r($bulan);
+		echo "<br>";
+		echo $array1[0];
+		echo "<br>";
+		echo $bulan[2];
+ ?>
