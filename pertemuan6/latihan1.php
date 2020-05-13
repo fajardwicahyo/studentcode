72cfcd10d41ba167a75a2e5f6a79b2aa7f4542df
Fajar Dwi Cahyo <44404657+fajardwicahyo@users.noreply.github.com>
2020-05-13 17:32:44 +0800

    associative array

---
+++ pertemuan6/latihan1.php
@@ -0,0 +1,41 @@
+<!-- <?php 	  
+		// $hari = ["Senin", "Selasa","Rabu", "Kamis", "Jumat", "Sabtu", "Ahad"];
+		// $bulan = array ("Januari", "Februari", "Maret");
+		// $arr = [100, "teks", True];
+		// array adalah pasangan dari key dan value
+		// key nya adalah index, dan dimulai dari 0
+		// var_dump($hari);
+		// echo "<br>";
+		// print_r($bulan);
+		// echo "<br>";
+		// echo $array1[0];
+		// echo "<br>";
+		// echo $arr[0];
+
+		
+		// <head>
+		// <html>
+		// 	<title>Latihan Array </title>
+		// 	<style>
+		// 		.kotak {
+		// 			width : 30px;
+		// 			height : 30px;
+		// 			backgound-color : #BADA55;
+		// 			margin : 3px;
+		// 			transition: 0.5s;
+		// 		}
+		// 		.kotak:hover {
+		// 			transform: rotate (180deg);
+		// 		}
+		// 	</style>
+		// </head>
+		// <body>
+ <?php
+$angka = [1,2,3],[4,5,6],[7,8,9];
+echo $angka [2][1];
+?>
+	<!-- <?php foreach($angka as $a) : ?>
+		<div class="kotak"> <?= $a; ?></div>
+		</body>
+	 	</html>-->
+ <!-- ?> -->
